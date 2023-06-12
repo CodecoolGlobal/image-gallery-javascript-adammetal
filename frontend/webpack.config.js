34a32324b6cbfc864264af5beed5b0274c4b4d32
adam.metal <adam.metal@codecool.com>
2023-06-12 11:03:15 +0200

    add webpack skeleton

---
+++ frontend/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'src');
+const OUT_DIR = path.join(__dirname, 'out');
+const PUB_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(SRC_DIR, 'index.js'),
+  output: {
+    path: OUT_DIR,
+    filename: 'dist.js'
+  },
+  devServer: {
+    static: {
+      directory: PUB_DIR
+    }
+  },
+  devtool: 'eval-cheap-source-map'
+}
